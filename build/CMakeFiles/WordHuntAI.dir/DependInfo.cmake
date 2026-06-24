71976ecc57a875aac9b57b1e3c04af525fad240d
Ynoj <117422590+notYnoj@users.noreply.github.com>
2026-06-23 19:29:06 -0500

    Add files via upload

---
+++ build/CMakeFiles/WordHuntAI.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ylracisbest/Desktop/quantProjects/WordHuntAI/src/cpp/DAT.cpp" "CMakeFiles/WordHuntAI.dir/src/cpp/DAT.cpp.o" "gcc" "CMakeFiles/WordHuntAI.dir/src/cpp/DAT.cpp.o.d"
+  "/Users/ylracisbest/Desktop/quantProjects/WordHuntAI/src/cpp/cnn.cpp" "CMakeFiles/WordHuntAI.dir/src/cpp/cnn.cpp.o" "gcc" "CMakeFiles/WordHuntAI.dir/src/cpp/cnn.cpp.o.d"
+  "/Users/ylracisbest/Desktop/quantProjects/WordHuntAI/src/cpp/cv.cpp" "CMakeFiles/WordHuntAI.dir/src/cpp/cv.cpp.o" "gcc" "CMakeFiles/WordHuntAI.dir/src/cpp/cv.cpp.o.d"
+  "/Users/ylracisbest/Desktop/quantProjects/WordHuntAI/src/cpp/engine.cpp" "CMakeFiles/WordHuntAI.dir/src/cpp/engine.cpp.o" "gcc" "CMakeFiles/WordHuntAI.dir/src/cpp/engine.cpp.o.d"
+  "/Users/ylracisbest/Desktop/quantProjects/WordHuntAI/src/cpp/filterWords.cpp" "CMakeFiles/WordHuntAI.dir/src/cpp/filterWords.cpp.o" "gcc" "CMakeFiles/WordHuntAI.dir/src/cpp/filterWords.cpp.o.d"
+  "/Users/ylracisbest/Desktop/quantProjects/WordHuntAI/src/cpp/main.cpp" "CMakeFiles/WordHuntAI.dir/src/cpp/main.cpp.o" "gcc" "CMakeFiles/WordHuntAI.dir/src/cpp/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/WordHuntAI.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WordHuntAI.dir/src/cpp/DAT.cpp.o"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/DAT.cpp.o.d"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/cnn.cpp.o"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/cnn.cpp.o.d"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/cv.cpp.o"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/cv.cpp.o.d"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/engine.cpp.o"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/engine.cpp.o.d"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/filterWords.cpp.o"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/filterWords.cpp.o.d"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/main.cpp.o"
+  "CMakeFiles/WordHuntAI.dir/src/cpp/main.cpp.o.d"
+  "WordHuntAI"
+  "WordHuntAI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WordHuntAI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
